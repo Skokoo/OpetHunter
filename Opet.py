380cfcc175a62e9dff73e88951808e053adf0d7f
Skokoo <nourriwilliam789@gmail.com>
2026-08-18 20:22:57 +0700

    Update Opet.py

--- Opet.py
+++ Opet.py
@@ -43,25 +43,25 @@ except ImportError:
     sys.exit(1)
 
 func_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "FUNC")
-        
-        if not os.path.exists(func_dir):
-            print(f"[\033[1mERROR\033[0m] The directory 'FUNC' not found in path: {func_dir}")
-        else:
-            import inspect
-            import importlib           
+
+    if not os.path.exists(func_dir):
+        print(f"[\033[1mERROR\033[0m] The directory 'FUNC' not found in path: {func_dir}")
+    else:
+        import inspect
+        import importlib           
             
-            import_targets = [
+        import_targets = [
                 ("Disasm", "Disasm"),
                 ("HexDump", "Hexdump"),
                 ("Strings", "StringsExtract"),
                 ("Analyze", "Analyze")
             ]            
             
-            for file_name, class_name in import_targets:
-                try:
-                    mod = importlib.import_module(f"FUNC.{file_name}")
-                    cls = getattr(mod, class_name)
-                    self.modules[class_name] = cls
+        for file_name, class_name in import_targets:
+            try:
+                mod = importlib.import_module(f"FUNC.{file_name}")
+                cls = getattr(mod, class_name)
+                self.modules[class_name] = cls
                 except Exception as e:
                     print(f"[\033[1mERROR\033[0m] Failed to import: {file_name}.{class_name}: {e}")
 
