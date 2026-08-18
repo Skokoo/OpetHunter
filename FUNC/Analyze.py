6f8fa3fc2cd86ce249f4ad80d171709c2e1f716a
Skokoo <nourriwilliam789@gmail.com>
2026-08-18 19:49:02 +0700

    Update Analyze.py

--- FUNC/Analyze.py
+++ FUNC/Analyze.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-class AnalyzeEngine:
+class Analyze:
     def __init__(self, shell_instance):
         self.shell = shell_instance
 
@@ -29,5 +29,31 @@ class AnalyzeEngine:
 
         if found_xrefs == 0:
             lines.append("[\033[1mERROR\033[0m] No external XREFs found for this address.")
+        lines.append("")
+        return "\n".join(lines)
+
+    def analyze_entropy_map(self):
+        lines = [f"\n[\033[1mINFO\033[0m] Shannon Entropy Analysis", "Block\tVirtual Addr\tScore\t\tStatus / Graph", "-" * 75]
+        block_size = 512
+
+        RED = self.shell.RED
+        YELLOW = self.shell.YELLOW
+        GREEN = self.shell.GREEN
+        BOLD = self.shell.BOLD
+        RESET = self.shell.RESET
+
+        for i in range(0, self.shell.file_size, block_size):
+            block = self.shell.binary_data[i:i+block_size]
+            entropy = self.shell.calculate_entropy(block)
+            bar_len = int(entropy * 4)
+            chart = "█" * bar_len
+            vaddr = self.shell.base_address + i
+
+            if entropy > 6.5:   
+                lines.append(f"#{i//block_size}\t{hex(vaddr)}\t{entropy:.2f}/8.0\t{RED}{BOLD}[PACKED] {RESET} {RED}{chart}{RESET}")
+            elif entropy > 4.5: 
+                lines.append(f"#{i//block_size}\t{hex(vaddr)}\t{entropy:.2f}/8.0\t{YELLOW}[CODE]   {RESET} {YELLOW}{chart}{RESET}")
+            else:               
+                lines.append(f"#{i//block_size}\t{hex(vaddr)}\t{entropy:.2f}/8.0\t{GREEN}[DATA]   {RESET} {GREEN}{chart}{RESET}")
         lines.append("")
         return "\n".join(lines)
