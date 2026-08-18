cadf82eb9598fc5a513a1d8ccb896ae4857b806a
Skokoo <nourriwilliam789@gmail.com>
2026-08-18 19:37:50 +0700

    Create HexDump.py

---
+++ FUNC/HexDump.py
@@ -0,0 +1,40 @@
+class HexdumpEngine:
+    def __init__(self, shell_instance):
+        self.shell = shell_instance
+
+    def run(self, args):
+        size = 128
+        if args and isinstance(args, list) and len(args) > 0:
+            try: size = int(args[0])
+            except: pass
+
+        chunk = self.shell.binary_data[self.shell.cursor : self.shell.cursor + size]
+        vaddr_start = self.shell.base_address + self.shell.cursor
+
+        WHITE = self.shell.WHITE
+        GREEN = self.shell.GREEN
+        RED   = self.shell.RED
+        RESET = self.shell.RESET
+
+        lines = [f"\n[\033[1mINFO\033[0m] Hex Dump at {hex(vaddr_start)}", f"  Offset      00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f   ASCII", "-" * 75]
+        for i in range(0, len(chunk), 16):
+            sub_chunk = chunk[i:i+16]
+            hex_str = ""
+            ascii_str = ""
+
+            for idx, b in enumerate(sub_chunk):
+                if idx == 8: hex_str += " "
+                if b == 0x00:
+                    hex_str += f"{WHITE}{b:02x}{RESET} "
+                    ascii_str += f"{WHITE}.{RESET}"
+                elif 0x20 <= b <= 0x7E:
+                    hex_str += f"{GREEN}{b:02x}{RESET} "
+                    ascii_str += f"{GREEN}{chr(b)}{RESET}"
+                else:
+                    hex_str += f"{RED}{b:02x}{RESET} "
+                    ascii_str += f"{RED}.{RESET}"
+
+            line_vaddr = vaddr_start + i
+            lines.append(f"  {hex(line_vaddr)}  {hex_str.ljust(60)}  {ascii_str}")
+        lines.append("-" * 75 + "\n")
+        return "\n".join(lines)
