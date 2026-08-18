e30a9e5c47ebcd26f7ce62e19a82c845950abbf5
Skokoo <nourriwilliam789@gmail.com>
2026-08-18 20:25:50 +0700

    Update Opet.py

--- Opet.py
+++ Opet.py
@@ -44,26 +44,28 @@ except ImportError:
 
 func_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "FUNC")
 
-    if not os.path.exists(func_dir):
-        print(f"[\033[1mERROR\033[0m] The directory 'FUNC' not found in path: {func_dir}")
-    else:
-        import inspect
-        import importlib           
-            
-        import_targets = [
-                ("Disasm", "Disasm"),
-                ("HexDump", "Hexdump"),
-                ("Strings", "StringsExtract"),
-                ("Analyze", "Analyze")
-            ]            
-            
-        for file_name, class_name in import_targets:
-            try:
-                mod = importlib.import_module(f"FUNC.{file_name}")
-                cls = getattr(mod, class_name)
-                self.modules[class_name] = cls
-                except Exception as e:
-                    print(f"[\033[1mERROR\033[0m] Failed to import: {file_name}.{class_name}: {e}")
+modules = {}
+
+if not os.path.exists(func_dir):
+    print(f"[\033[1mERROR\033[0m] The directory 'FUNC' not found in path: {func_dir}")
+else:
+    import inspect
+    import importlib           
+
+    import_targets = [
+        ("Disasm", "Disasm"),
+        ("HexDump", "Hexdump"),
+        ("Strings", "StringsExtract"),
+        ("Analyze", "Analyze")
+    ]            
+
+    for file_name, class_name in import_targets:
+        try:
+            mod = importlib.import_module(f"FUNC.{file_name}")
+            cls = getattr(mod, class_name)
+            modules[class_name] = cls
+        except Exception as e:
+            print(f"[\033[1mERROR\033[0m] Failed to import: {file_name}.{class_name}: {e}")
 
 RESET   = "\033[0m"
 BOLD    = "\033[1m"
