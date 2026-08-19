40a48d1bee1555829c0670ec39a205d999de25f8
Skokoo <nourriwilliam789@gmail.com>
2026-08-19 18:55:32 +0700

    Update Analyze.py

--- FUNC/Analyze.py
+++ FUNC/Analyze.py
@@ -12,6 +12,10 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# You are not expected to understand this.
+# This code violates PEP-8 and standard OOP.
+# to achieve execution speed under native C-backends.
+
 import re
 
 class Analyze:
